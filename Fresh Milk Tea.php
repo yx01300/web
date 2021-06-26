10786bc496ee5e0240bba65901e9d7188dc950dc
gracekuo90 <73097885+gracekuo90@users.noreply.github.com>
2021-06-26 22:47:15 +0800

    Update Fresh Milk Tea.php

--- Fresh Milk Tea.php
+++ Fresh Milk Tea.php
@@ -48,7 +48,6 @@
     </section>
     <div id="content">
       <section class="sidebar">
-        <input type="text" id="search" value="search">
         <div id="menubar">
           <nav class="menu">
             <h2>
